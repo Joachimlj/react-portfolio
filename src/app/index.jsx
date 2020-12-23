d76452e8dd3049c95429d1d407d0e656f928bd6c
Joachim Jørgensen <joachimlj.work@gmail.com>
2020-12-23 20:13:10 +0100

    Added react router and created a homepage component

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
+import Homepage from "./pages/home/";
+
 export default function App() {
   return (
     <>
-      <h1>welcome to the app</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe minima
-        voluptate placeat magni rerum aliquam, beatae animi ad reprehenderit
-        ipsum ratione molestias accusantium, velit illo quis earum delectus
-        adipisci quasi!
-      </p>
+      <Homepage />
     </>
   );
 }
---
+++ src/app/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe minima
+        voluptate placeat magni rerum aliquam, beatae animi ad reprehenderit
+        ipsum ratione molestias accusantium, velit illo quis earum delectus
+        adipisci quasi!
+      </p>
+    </>
+  );
+}
