99e938c4d027a58744b78e3f086c130c1baf586c
Joachim Jørgensen <joachimlj.work@gmail.com>
2020-12-23 18:50:17 +0100

    initial file clean up

---
+++ src/app/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1>welcome to the app</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe minima
+        voluptate placeat magni rerum aliquam, beatae animi ad reprehenderit
+        ipsum ratione molestias accusantium, velit illo quis earum delectus
+        adipisci quasi!
+      </p>
+    </>
+  );
+}
