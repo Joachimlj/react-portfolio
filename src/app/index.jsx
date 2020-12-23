0bb6365f56413b31598c5de4b869056e41681fb0
Joachim Jørgensen <joachimlj.work@gmail.com>
2020-12-23 20:19:23 +0100

    Created about page

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/home/";
+import Aboutpage from "./pages/about/";
 
 export default function App() {
   return (
     <>
-      <Homepage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/about">
+            <Aboutpage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/app/pages/about/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Aboutpage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe minima
+        voluptate placeat magni rerum aliquam, beatae animi ad reprehenderit
+        ipsum ratione molestias accusantium, velit illo quis earum delectus
+        adipisci quasi!
+      </p>
+    </>
+  );
+}
